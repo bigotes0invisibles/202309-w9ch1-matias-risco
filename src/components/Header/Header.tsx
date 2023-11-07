943d6c39c9f3ffd43b16a42fca1e0c7d84a8d4cd
Matias Risco <matiasriscolagos@gmail.com>
2023-11-07 14:03:57 +0100

    Add styles Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import HeaderStyled from "./HeaderStyled";
 const Header = (): React.ReactElement => {
   return (
     <HeaderStyled>
-      <h1>Flims</h1>
+      <h1>PrieMov</h1>
     </HeaderStyled>
   );
 };
