a3f4776a5dd673c768906aa4deeeb4647d79fd75
Matias Risco <matiasriscolagos@gmail.com>
2023-11-08 16:05:41 +0100

    Fix test MovieList component

--- src/components/MoviesList/MoviesList.test.tsx
+++ src/components/MoviesList/MoviesList.test.tsx
@@ -1,27 +1,36 @@
-import { screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
-import { renderWithProviders } from "../../utils/testUtils";
+import { Provider } from "react-redux";
 import mockMovies from "../../mocks/mockData";
 import mainTheme from "../../styles/mainTheme";
 import MoviesList from "./MoviesList";
+import { setupStore } from "../../utils/setUpStore";
 
 describe("Given the component MoviesList", () => {
   describe("When it is render and have alredy pulled information of store", () => {
     test("then it should show the titles of the movies", () => {
-      renderWithProviders(
-        <ThemeProvider theme={mainTheme}>
-          <MoviesList />
-        </ThemeProvider>,
-        {
-          preloadedState: {
-            moviesState: { movies: mockMovies },
-          },
-        },
+      const mockstore = setupStore({
+        moviesState: { movies: mockMovies },
+      });
+      const firstMovieName = mockMovies[0].name;
+      const secondMovieName = mockMovies[1].name;
+      render(
+        <Provider store={mockstore}>
+          <ThemeProvider theme={mainTheme}>
+            <MoviesList />
+          </ThemeProvider>
+        </Provider>,
       );
 
-      const HeadingElements = screen.getAllByRole("heading");
+      const firstMovieElement = screen.getByRole("heading", {
+        name: firstMovieName,
+      });
+      const secondMovieElement = screen.getByRole("heading", {
+        name: secondMovieName,
+      });
 
-      expect(HeadingElements.length).toBe(2);
+      expect(firstMovieElement).toBeInTheDocument();
+      expect(secondMovieElement).toBeInTheDocument();
     });
   });
 });
