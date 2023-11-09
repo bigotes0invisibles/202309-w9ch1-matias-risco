947f28c781d508fba307e24454dfb1321f7c4727
Matias Risco <matiasriscolagos@gmail.com>
2023-11-09 20:04:30 +0100

    Add styles for  component FormMovie

--- src/components/FormMovie/FormMovie.tsx
+++ src/components/FormMovie/FormMovie.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { MoviesStructure } from "../../store/feature/movies/types";
+import FormMovieStyled from "./FormMovieStyled";
 
 interface FormMoviePropsStructure {
   onSumbit: (movie: MoviesStructure) => void;
@@ -12,7 +13,8 @@ const FormMovie = ({
     duration: "",
     genres: [""],
     id: 0,
-    image: "",
+    image:
+      "https://st3.depositphotos.com/1064045/15061/i/450/depositphotos_150614902-stock-photo-unusual-cinema-concept-3d-illustration.jpg",
     mainProtagonist: "",
     meme: "no",
     name: "",
@@ -47,57 +49,77 @@ const FormMovie = ({
   };
 
   return (
-    <form onSubmit={onSumbitForm}>
-      <label htmlFor="name">name: </label>
-      <input
-        type="text"
-        id="name"
-        onChange={onChange}
-        value={newMovie.name}
-        required
+    <FormMovieStyled onSubmit={onSumbitForm} className="form-movie">
+      <div className="form-movie__input">
+        <label htmlFor="name">name: </label>
+        <input
+          type="text"
+          id="name"
+          onChange={onChange}
+          value={newMovie.name}
+          required
+        />
+      </div>
+      <div className="form-movie__input">
+        <label htmlFor="duration">duration:</label>
+        <input
+          type="time"
+          id="duration"
+          onChange={onChange}
+          value={newMovie.duration}
+          required
+        />
+      </div>
+      <div className="form-movie__input">
+        <label htmlFor="mainProtagonist">main protagonist:</label>
+        <input
+          type="text"
+          onChange={onChange}
+          id="mainProtagonist"
+          value={newMovie.mainProtagonist}
+          required
+        />
+      </div>
+      <div className="form-movie__input">
+        <label htmlFor="watched">watched: </label>
+        <input
+          type="checkbox"
+          checked={newMovie.watched}
+          id="watched"
+          onChange={onChange}
+          className="form-movie__checkbox"
+          required
+        />
+      </div>
+      <div className="form-movie__input">
+        <label htmlFor="genres">genres:</label>
+        <input
+          type="text"
+          value={newMovie.genres.join(",")}
+          id="genres"
+          onChange={onChange}
+          required
+        />
+      </div>
+      <div className="form-movie__input">
+        <label htmlFor="image">image:</label>
+        <input
+          type="url"
+          value={newMovie.image}
+          id="image"
+          onChange={onChange}
+          required
+        />
+      </div>
+      <img
+        src={newMovie.image}
+        alt={`the movie ${newMovie.name}`}
+        width="300"
+        height="150"
+        className="form-movie__image"
       />
-      <label htmlFor="duration">duration:</label>
-      <input
-        type="time"
-        id="duration"
-        onChange={onChange}
-        value={newMovie.duration}
-        required
-      />
-      <label htmlFor="mainProtagonist">main protagonist:</label>
-      <input
-        type="text"
-        onChange={onChange}
-        id="mainProtagonist"
-        value={newMovie.mainProtagonist}
-        required
-      />
-      <label htmlFor="watched">watched: </label>
-      <input
-        type="checkbox"
-        checked={newMovie.watched}
-        id="watched"
-        onChange={onChange}
-        required
-      />
-      <label htmlFor="image">image:</label>
-      <input
-        type="url"
-        value={newMovie.image}
-        id="image"
-        onChange={onChange}
-        required
-      />
-      <label htmlFor="genres">genres:</label>
-      <input
-        type="text"
-        value={newMovie.genres.join(",")}
-        id="genres"
-        onChange={onChange}
-        required
-      />
-      <button>Hello it's is you who i am lokking for</button>
-    </form>
+      <button className="form-movie__button">Add new movie</button>
+    </FormMovieStyled>
   );
 };
 
