d7981dab1fb7154efa6f305fcb4aad6bfb6d1471
Matias Risco <matiasriscolagos@gmail.com>
2023-11-09 17:10:49 +0100

    Add mockApiData to setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,23 @@
 import "@testing-library/jest-dom";
+import { server } from "./mocks/main";
+
+import mockMovies from "./mocks/mockData";
+import { MoviesStructure } from "./store/feature/movies/types";
+
+export let moviesApi: MoviesStructure[] = mockMovies;
+
+beforeAll(() => {
+  moviesApi = mockMovies;
+  return server.listen();
+});
+
+afterEach(() => {
+  moviesApi = mockMovies;
+  return server.resetHandlers();
+});
+
+afterAll(() => server.close());
+
+server.events.on("request:start", ({ request }) => {
+  console.log("MSW intercepted:", request.method, request.url);
+});
